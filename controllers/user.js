413c79e0ea12bade9bb69d960aad1051472256a8
Lewis Kabui <lewis.maina@andela.com>
2017-04-30 17:14:40 +0300

    Create GET (list) operation fir User resource

--- controllers/user.js
+++ controllers/user.js
@@ -12,3 +12,11 @@ exports.postUsers = function(req, res) {
         res.json({message: 'New beer drinker added to the locker room!'});
     });
 };
+
+exports.getUsers = function(req, res) {
+    User.find(function(err, users) {
+        if (err)
+            res.send(err);
+        res.json(users);
+    });
+};
