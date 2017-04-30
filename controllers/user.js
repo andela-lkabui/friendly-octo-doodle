b7836c0b19af4878e91706f96f55c627c57cca01
Lewis Kabui <lewis.maina@andela.com>
2017-04-30 17:11:49 +0300

    Create POST for User operation resource

---
+++ controllers/user.js
@@ -0,0 +1,14 @@
+var User = require('../models/user');
+
+exports.postUsers = function(req, res) {
+    var user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    user.save(function(err) {
+        if (err)
+            res.send(err);
+        res.json({message: 'New beer drinker added to the locker room!'});
+    });
+};
