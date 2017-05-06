8ac33f46fc0543fa33a50bb53f3d5d29cf5c5547
Lewis Kabui <lewis.maina@andela.com>
2017-05-06 22:37:29 +0300

    Create Client authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('../models/user');
+var Client = require('../models/client');
 
 passport.use(new BasicStrategy(
     function(username, password, callback) {
@@ -28,4 +29,18 @@ passport.use(new BasicStrategy(
     }
 ));
 
+passport.use('client-basic', new BasicStrategy(
+    function(username, password, callback) {
+        Client.findOne({ id: username }, function(err, client) {
+            if (err) {
+                return callback(err);
+            }
+            if (!client || client.secret !== password) {
+                return callback(null, false);
+            }
+            return callback(null, client);
+        });
+    }));
+
 exports.isAuthenticated = passport.authenticate('basic', { session: false });
+exports.isClientAuthenticated = passport.authenticate('client-basic', { session: false });
