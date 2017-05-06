267f3b58ed42baff46c700540cd034870e7a177b
Lewis Kabui <lewis.maina@andela.com>
2017-05-06 22:58:16 +0300

    Create BearerStrategy
    * This strategy allows the app to accept requests from application
    clients using OAuth tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 var passport = require('passport');
+var BearerStrategy = require('passport-http-bearer').Strategy;
 var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('../models/user');
 var Client = require('../models/client');
+var Token = require('../models/token');
 
 passport.use(new BasicStrategy(
     function(username, password, callback) {
@@ -42,5 +44,28 @@ passport.use('client-basic', new BasicStrategy(
         });
     }));
 
+passport.use(new BearerStrategy(
+    function(accessToken, callback) {
+        Token.findOne({ value: accessToken }, function(err, token) {
+            if (err) {
+                return callback(err);
+            }
+            if (!token) {
+                return callback(null, false);
+            }
+            User.findOne({ _id: token.userId }, function(err, user) {
+                if (err) {
+                    return callback(err);
+                }
+                if (!user) {
+                    return callback(null, false);
+                }
+                callback(null, user, { scope: '*'});
+            });
+        });
+    }
+));
+
 exports.isAuthenticated = passport.authenticate('basic', { session: false });
 exports.isClientAuthenticated = passport.authenticate('client-basic', { session: false });
+exports.isBearerAuthenticated = passport.authenticate('bearer', { session: false });
